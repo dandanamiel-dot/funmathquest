f0db5cca4f72aa06baef0c5a66a58873b7c2b2c1
Dan Amiel <dandanamiel@gmail.com>
2026-02-04 11:33:26 +0200

    Initial Commit

---
+++ FunMathQuest/FunMathQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunMathQuestApp.swift
+//  FunMathQuest
+//
+//  Created by Dan Amiel on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FunMathQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
